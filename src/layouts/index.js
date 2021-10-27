eb89106d3a41fe71ea756e0ed7870a1ab62f0ada
Chrystal Lantink <hello@chrystalgriffiths.com>
2021-10-27 12:06:58 +0200

    Re-add chrystal font

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,10 +5,10 @@ import Helmet from 'react-helmet'
 import Header from '../components/Header'
 import './index.css'
 
-import '../../public/fonts/chrystal/chrystal.css'
 import '../../public/fonts/devicons/devicons.css'
 import '../../public/fonts/fontawesome/font-awesome.css'
 
+import '../fonts/chrystal/stylesheet.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
